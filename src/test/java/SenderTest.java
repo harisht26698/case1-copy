469655cdf7b55d557c826d919a6303a3fd6f68b8
harisht26698 <harish.t@philips.com>
2020-09-25 14:08:29 +0530

    exception removed in sender test

--- src/test/java/SenderTest.java
+++ src/test/java/SenderTest.java
@@ -2,21 +2,19 @@ import com.philips.sender.service.DataProcessing;
 import com.philips.sender.service.PrintToConsole;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-
 import static org.junit.Assert.*;
 
 public class SenderTest {
     private String testLine = "ReviewDate,Comments";
     private String[] testArray = testLine.split(",");
     @Test
-    public void checkPrintToConsoleIsColoumnExists() throws FileNotFoundException {
+    public void checkPrintToConsoleIsColoumnExists(){
         assertTrue(PrintToConsole.isColumnExists(0));
         assertFalse(PrintToConsole.isColumnExists(-1));
     }
 
     @Test
-    public void checkCsvLineToArrayOfString() throws FileNotFoundException {
+    public void checkCsvLineToArrayOfString(){
 
         String[] retArray = DataProcessing.csvLineToArrayOfString(testLine);
         assertEquals(testArray[0],retArray[0]);
@@ -24,14 +22,14 @@ public class SenderTest {
     }
 
     @Test
-    public void checkGetColumnNumber() throws FileNotFoundException {
+    public void checkGetColumnNumber(){
         assertEquals(0,DataProcessing.getColumnNumber(testArray,"ReviewDate"));
         assertEquals(1,DataProcessing.getColumnNumber(testArray,"Comments"));
         assertEquals(-1,DataProcessing.getColumnNumber(testArray,"wrongCol"));
     }
 
     @Test
-    public void checkValidateColumnNumber() throws FileNotFoundException {
+    public void checkValidateColumnNumber(){
         assertEquals(-1,DataProcessing.validateColumnNumber(2,2));
         assertEquals(0,DataProcessing.validateColumnNumber(2,0));
         assertEquals(-1,DataProcessing.validateColumnNumber(2,3));
